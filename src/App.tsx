4c0ce396e35fedf282de63ec8ded20864a76aada
MikkoVasankari <vasankarimikko@gmail.com>
2023-04-24 12:32:46 +0300

    Project initialized

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div >
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        text here
+      </p>
+     
+      <p className="read-the-docs">
+        text here
+      </p>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="name">Player Realm</label>
+        <input type="text" id="Realm" />
+        <br></br>
+        <label htmlFor="name">Player Name</label>
+        <input type="text" id="name" />
+        <br></br>
+        <br></br>
+        <button >Submit</button>
+      </form>
+
+    </>
+  )
+}
+
+export default App
